d3177c8b540a7ef2a585ebd7e898aae299bbcaa7
Elias De La Cruz <exdj0002@ce.pucmm.edu.do>
2026-01-23 11:25:22 -0400

    Revert "feat:service-worker"

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,4 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
 };
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  scope: "/app",
-  sw: "service-worker.js",
-});
-
-export default withPWA(nextConfig);
+export default nextConfig;
